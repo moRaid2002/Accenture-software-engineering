22226d0e6990f8192be7eb1196ff6366ce0e6417
Mohamed Raid Athamena <101266464+moRaid2002@users.noreply.github.com>
2024-09-02 04:54:21 +0400

    task 1
    implementing the SearchController using test driven development (TDD)

--- src/main/java/com/mockcompany/webapp/controller/SearchController.java
+++ src/main/java/com/mockcompany/webapp/controller/SearchController.java
@@ -82,12 +82,23 @@ public class SearchController {
 
         Iterable<ProductItem> allItems = this.productItemRepository.findAll();
         List<ProductItem> itemList = new ArrayList<>();
+        boolean isExactMatch = query.startsWith("\"") && query.endsWith("\"");
+        if (isExactMatch) query = query.substring(1, query.length()-1);
 
         // This is a loop that the code inside will execute on each of the items from the database.
         for (ProductItem item : allItems) {
             // TODO: Figure out if the item should be returned based on the query parameter!
-            boolean matchesSearch = true;
-            itemList.add(item);
+            String name = item.getName().toLowerCase();
+            String des = item.getDescription().toLowerCase();
+            String lowerQuery = query.toLowerCase();
+            if(isExactMatch){
+                if (name.equals(lowerQuery) || des.equals(lowerQuery)) itemList.add(item);
+            }else{
+                if(name.contains(lowerQuery)|| des.contains(lowerQuery)) itemList.add(item);
+            }
+            
+            
+            
         }
         return itemList;
     }
